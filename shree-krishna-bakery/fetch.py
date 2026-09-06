6b12a9aad5a09c499440902a73306f630101a5ef
brijesh j.kanani <brizpatel@Mac.lan>
2026-09-06 22:13:26 +0530

    Remove bakery site from portfolio deployment

--- shree-krishna-bakery/fetch.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "https://shreekrishnabakery.com/menu.php"
-headers = {'User-Agent': 'Mozilla/5.0'}
-response = requests.get(url, headers=headers)
-with open("menu_full.html", "w") as f:
-    f.write(response.text)
